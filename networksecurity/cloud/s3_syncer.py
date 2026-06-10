26b3d79861e63e62c375c986e778caa6e08abc8c
Pragathithimmaiah <dtpragathi@gmail.com>
2026-06-11 01:07:59 +0530

    final model

--- networksecurity/cloud/s3_syncer.py
+++ networksecurity/cloud/s3_syncer.py
@@ -1,12 +1,18 @@
-
 import os
 
-
 class S3Sync:
-    def sync_folder_to_s3(self,folder,aws_bucket_url):
-        command = f"aws s3 sync {folder} {aws_bucket_url} "
-        os.system(command)
+    def sync_folder_to_s3(self, folder, aws_bucket_url):
+        command = f'aws s3 sync "{folder}" "{aws_bucket_url}"'
+        print("Running:", command)
 
-    def sync_folder_from_s3(self,folder,aws_bucket_url):
-        command = f"aws s3 sync  {aws_bucket_url} {folder} "
-        os.system(command)
+        result = os.system(command)
+
+        print("Exit code:", result)
+
+    def sync_folder_from_s3(self, folder, aws_bucket_url):
+        command = f'aws s3 sync "{aws_bucket_url}" "{folder}"'
+        print("Running:", command)
+
+        result = os.system(command)
+
+        print("Exit code:", result)
